6955f8bde482228c1734797ca96617ba1baadc8a
Animesh Kumar <animesh0721@gmail.com>
2017-09-06 11:39:55 +0530

    Adds InAppBrowser plugin to open the url.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+// import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  browser: any;
+
+  constructor(private iab: InAppBrowser) {
 
   }
 
+  ionViewDidLoad() {
+    console.log('View loaded');
+    let iabOptions: InAppBrowserOptions = {
+      location: 'no',
+      zoom: 'no',
+      shouldPauseOnSuspend: 'yes',
+      closebuttoncaption: '',
+      toolbar: 'no',
+      enableViewportScale: 'yes',
+      suppressesIncrementalRendering: 'yes',
+      fullscreen: 'yes'
+    };
+    this.browser = this.iab.create("http://kimtutoring.com/mobile/", "_self", iabOptions);
+    this.browser.show();
+  }
+
+  ionViewWillLeave() {
+    this.browser.close();
+  }
 }
